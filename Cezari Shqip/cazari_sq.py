14ff269d08bce71a532e46d08f7358c2d06930a3
xhepi6 <xhkrasniqi98@gmail.com>
2019-03-24 01:45:08 +0100

    User Interface Implementation

--- Cezari Shqip/cazari_sq.py
+++ Cezari Shqip/cazari_sq.py
@@ -1,16 +1,19 @@
 #OVERCOMMENTED WHATEVER...
 #Importimi i funksionit per te dalluar shkronjat e dyfishta ne gjuhen shqipe
 from shkronje_edyfisht import shkronje_edyfisht
+import os
 
 #Alfabeti i gjuhes shqipe
 alfabeti = ['A','B','C','Ç','D','DH','E','Ë','F','G','GJ','H','I','J','K','L','LL','M','N','NJ','O','P','Q','R','RR','S','SH','T','TH','U','V','X','XH','Y','Z','ZH']
 
 #Funksioni qe merr tekstin dhe qelesin dhe kthen tekstin e enkriptuar
 def cezari_shqip(teksti,qelsi):
+    print('brenda cezari_shqip()')
     list_cezari = []
 
     tejkalo = False
     for shkronje in list(teksti.upper()):
+        print('shkronja qe po enkriptohet:', shkronje)
         #Neqoftese kjo variabel eshte true 'iteratori' eshte momentalisht ne
         #  shkronje te dyfisht qe veqse eshte enkriptuar
         if tejkalo == True:
@@ -28,8 +31,9 @@ def cezari_shqip(teksti,qelsi):
                 nralf_zevendsimit = nralf_zevendsimit + 1
                 #Ky kusht sherben kur numri alfabetik tejkalon numrin e shkronjave te alfabetit
                 #prandaj kur tejkalon kthehemi prap nga fillimi i alfabetit(per shkronja te dyfishta)
-                if nralf_zevendsimit>36:
-                    nralf_zevendsimit = nralf_zevendsimit - 36
+                if nralf_zevendsimit>35:
+                    nralf_zevendsimit = nralf_zevendsimit - 35
+                print('shkronja u enkriptua me: ', alfabeti[nralf_zevendsimit])
                 list_cezari.append(alfabeti[nralf_zevendsimit])
                 # nje variabel qe tregon se shkronja momentale eshte pjes e shkronjes se dyfisht
                 #  dhe duhet tejkaluar
@@ -37,20 +41,48 @@ def cezari_shqip(teksti,qelsi):
             else:
                 #Ky kusht sherben kur numri alfabetik tejkalon numrin e shkronjave te alfabetit
                 #prandaj kur tejkalon kthehemi prap nga fillimi i alfabetit
-                if nralf_zevendsimit>36:
-                    nralf_zevendsimit = nralf_zevendsimit - 36
+                if nralf_zevendsimit>35:
+                    nralf_zevendsimit = nralf_zevendsimit - 35
+                print('shkronja u enkriptua me: ',alfabeti[nralf_zevendsimit])
                 list_cezari.append(alfabeti[nralf_zevendsimit])
         except ValueError:
             #Nese karakteri nje tekst nuk eshte pjes e alfabetit
             # nuk enkriptohet por thjesht shtohet ne tekstin e enkriptuar
+            print('shkronja nuk u zevendesua ', shkronje)
             list_cezari.append(shkronje)
+    print('enkriptimi i tekstit perfundoi')
     return list_cezari
 
+def enkripto_dokumentin(path, filename, qelesi, enc_dec):
+    print('brenda funksionit per enkriptim')
+    if enc_dec == False:
+        qelesi = qelesi * (-1)
+    print('brenda enkripto_dokuntin() ', path)
+    txt_file = open(path,'r')
+    print('txt_file u hap')
+    text = txt_file.read()
+    txt_file.close()
+    print('txt_file u mbyll')
+    #funksioni i enkriptimit kthen list
+    teksti_list_enkriptuar = cezari_shqip(text, qelesi)
+    print('teksti u enkriptua')
+    #bashkojme listen
+    teksti_enkriptuar = ''.join(teksti_list_enkriptuar)
+    directory = os.path.split(path)[0]
+    enc_file = open( os.path.join(directory,filename),'w')
+    print('u krijua file i ri')
+    enc_file.write(teksti_enkriptuar)
+    enc_file.close()
+    print('file i ri u mbyll')
+
+
+"""
 try:
     #Nese shtyp diqka tjeter perveq numer qelesi merr vleren 2
     qelesi = int(input('Shkruani qelesin: '))
 except ValueError:
     qelesi = 2
-teksti = input('Teksi qe deshironi te enkriptoni: ')
-teksti_enkriptuar = cezari_shqip(teksti, qelesi)
-print(teksti_enkriptuar)
+"""
+
+#teksti_enkriptuar = cezari_shqip(teksti, qelesi)
+
--- Cezari Shqip/shkronje_edyfisht.py
+++ Cezari Shqip/shkronje_edyfisht.py
@@ -1,38 +1,49 @@
+import os
 
+def directory_edit(path):
+    
+    directory = os.path.split(path)
+    
+    return directory
+
+
+    
 
 def shkronje_edyfisht(shkronja,fjalia):
-    shkronja = shkronja.upper()
-    fjalia = list(fjalia.upper())
-    nr = fjalia.index(shkronja)
-    if shkronja == 'D':
-        if fjalia[nr+1] == 'H':
-            return True
-    elif shkronja == 'G':
-        if fjalia[nr+1] == 'J':
-            return True
-    elif shkronja == 'L':
-        if fjalia[nr+1] == 'L':
-            return True
-    elif shkronja == 'N':
-        if fjalia[nr+1] == 'J':
-            return True
-    elif shkronja == 'R':
-        if fjalia[nr+1] == 'R':
-            return True
-    elif shkronja == 'S':
-        if fjalia[nr+1] == 'H':
-            return True
-    elif shkronja == 'T':
-        if fjalia[nr+1] == 'H':
-            return True
-    elif shkronja == 'X':
-        if fjalia[nr+1] == 'H':
-            return True
-    elif shkronja == 'Z':
-        if fjalia[nr+1] == 'H':
-            return True
-    else:
-        return False
-    
+    try:
+        shkronja = shkronja.upper()
+        fjalia = list(fjalia.upper())
+        nr = fjalia.index(shkronja)
+        if shkronja == 'D':
+            if fjalia[nr+1] == 'H':
+                return True
+        elif shkronja == 'G':
+            if fjalia[nr+1] == 'J':
+                return True
+        elif shkronja == 'L':
+            if fjalia[nr+1] == 'L':
+                return True
+        elif shkronja == 'N':
+            if fjalia[nr+1] == 'J':
+                return True
+        elif shkronja == 'R':
+            if fjalia[nr+1] == 'R':
+                return True
+        elif shkronja == 'S':
+            if fjalia[nr+1] == 'H':
+                return True
+        elif shkronja == 'T':
+            if fjalia[nr+1] == 'H':
+                return True
+        elif shkronja == 'X':
+            if fjalia[nr+1] == 'H':
+                return True
+        elif shkronja == 'Z':
+            if fjalia[nr+1] == 'H':
+                return True
+        else:
+            return False
+    except IndexError:
+        print('shkronja e fundit')    
     
         
